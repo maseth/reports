46562f7909dcb2ca580d215c5f140f381da7ba3a
maseth <maciej.gregorczyk@icloud.com>
2018-11-07 20:39:38 +0100

    aqq

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import datetime as dt
 
+absence = 0.147
 df = pd.DataFrame()
 df['start'] = [dt.datetime(2018, 5, 1), dt.datetime(2018, 11, 1), dt.datetime(2018, 1, 1)]
 df['end'] = [dt.datetime(2018, 11, 30), dt.datetime(2018, 11, 30), dt.datetime(2018, 12, 31) ]
@@ -12,12 +13,11 @@ for row in df.itertuples():
     s = len(time_range)
     data_series += (zip(time_range,  [row.hours/s]*s))
 
-columns_names = ['date',  'hours']
-df2 = pd.DataFrame(data_series, columns=columns_names)
-
+df2 = pd.DataFrame(data_series, columns={'date', 'hours'})
 df2 = df2.groupby('date').sum().groupby(pd.Grouper(freq='M')).agg(['sum', 'count'])
 df2.columns = ['hours', 'count_days']
-df2['people'] = df2['hours']/(df2['count_days']*8)
+people = df2['hours'] / (df2['count_days'] * 8)
+df2['people'] = people + (people * absence)
 df2.reset_index()
 
 print(df2)
